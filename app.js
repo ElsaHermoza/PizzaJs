3027b62a54edb00d6b8b250551785df295183306
ElsaH <evihermoza@hotmail.com>
2024-05-23 11:00:34 +0200

    init pizza exe

---
+++ app.js
@@ -0,0 +1,28 @@
+
+//let pasosPizza = ['Estirar la masa', 'Agregar la salsa de tomate', 'Agregar queso mozzarella', 
+//'Agregar el peperoni', 'Agregar oregano al gusto','Hornear por 30mins']
+let recetaMasa = ['30grs levadura', '2 1/2 tzs agua', '1kg harina', '1cdta sal', '2cds aceite'];
+let ingredientes = ['Salsa de tomate', 'Queso mozarella rallado', 'oregano molido', 'peperoni'];
+
+
+let pedido = true;
+
+let amasar = "mezclar ";
+function preparacion(){
+
+    console.log(`Preparando masa: ${amasar + recetaMasa}`);
+   
+}
+
+
+//else
+    //console.log('Sin Pedidos');
+
+
+
+
+
+//console.log(amasar+recetaMasa);
+
+    
+//let paso1 = "mezclar 2 1/2 tzas de agua tibia con 30 gramos de levadura fresca"
